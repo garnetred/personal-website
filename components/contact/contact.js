279135b7352f359ab5fbefdf601d4a6f672e1d15
December G <dgarnetsmith@gmail.com>
2023-04-02 00:23:04 -0500

    Remove colon after each field name in contact form

--- components/contact/contact.js
+++ components/contact/contact.js
@@ -68,7 +68,7 @@ export default function ContactPage() {
               onSubmit={submitForm}
             >
               <input type="hidden" name="bot-field" />
-              <p className="contact-form-labels">Name:</p>
+              <p className="contact-form-labels">Name</p>
               <input type="hidden" name="form-name" value="contact-form" />
               <input
                 className="name-input"
@@ -80,7 +80,7 @@ export default function ContactPage() {
                 onChange={(e) => handleChange(e)}
                 required
               />
-              <p className="contact-form-labels">Email:</p>
+              <p className="contact-form-labels">Email</p>
               <input
                 className="email-input"
                 type="text"
@@ -91,7 +91,7 @@ export default function ContactPage() {
                 onChange={(e) => handleChange(e)}
                 required
               />
-              <p className="contact-form-labels">Message:</p>
+              <p className="contact-form-labels">Message</p>
               <textarea
                 className="message-input"
                 type="textarea"
