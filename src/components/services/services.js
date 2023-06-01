ca39ad889a59e85ac71a91ba211757033fd59183
December G <dgarnetsmith@gmail.com>
2023-06-01 18:54:44 -0500

    Merge changes in services

--- src/components/services/services.js
+++ src/components/services/services.js
@@ -5,15 +5,10 @@ export default function ServicesPage() {
   return (
     <>
       <Seo useTitleTemplate={true} />
-<<<<<<< HEAD
       <Section heading="Services" anchor="services">
         <p className="services-details">
           I specialize in offering the below services:
         </p>
-=======
-      <Section heading="Services" anchor="services" className="hi">
-        <p className="services-details">I specialize in the below services:</p>
->>>>>>> d58c9d0f40e50dd571ae6e9c5d12723d464c10df
         <ul>
           <li>automating data entry via programming</li>
           <li>maintaining existing websites or legacy applications</li>
@@ -24,11 +19,7 @@ export default function ServicesPage() {
         </ul>
         <p className="services-details">
           Please reach out via the <a href="/#contact">contact form</a> if you
-<<<<<<< HEAD
-          would like to learn more.
-=======
           would like to schedule a consultation.
->>>>>>> d58c9d0f40e50dd571ae6e9c5d12723d464c10df
         </p>
       </Section>
     </>
